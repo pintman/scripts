f1c1005ef0ff0227f675fb1bf64523011cb284a6
Marco Bakera <Marco@Bakera.de>
2015-07-30 19:42:16 +0200

    changed to download only one.

--- bin/vnc_viewer_jar.sh
+++ bin/vnc_viewer_jar.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 # use -x for debugging
 
-TMP=$(mktemp -d)
+DIR=$(dirname $0)/vnc_viewer_jar
 
-# download vnc viewer application with curl
-curl http://www.tightvnc.com/download/1.3.10/tightvnc-1.3.10_javabin.zip > $TMP/vncv.zip
-# unzip into temp folder
-unzip -q -d $TMP $TMP/vncv.zip
+if [[ ! ( -d $DIR ) ]]
+then
+    mkdir $DIR
+    # download vnc viewer application with curl
+    curl http://www.tightvnc.com/download/1.3.10/tightvnc-1.3.10_javabin.zip > $DIR/vncv.zip
+    # unzip into temp folder
+    unzip -q -d $DIR $DIR/vncv.zip
+    rm $DIR/vncv.zip
+fi
 
 # start viewer
 #java -cp classes/VncViewer.jar VncViewer HOST localhost
-java -cp $TMP/classes/VncViewer.jar VncViewer HOST localhost PORT 5901
+java -cp $DIR/classes/VncViewer.jar VncViewer HOST localhost PORT 5901
 
-
-rm -rf $TMP
