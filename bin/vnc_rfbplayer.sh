c3f46b8ea537d4618400e1a0dc03b21f0d4a8cb9
Marco Bakera <Marco@Bakera.de>
2015-07-30 13:57:30 +0200

    Added new script for playing rfb files. These file have been recorded with i.e. the tightvnc java standalone application.

---
+++ bin/vnc_rfbplayer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+# use -x for debugging
+
+TMP=$(mktemp -d)
+
+# download vnc player application with curl
+curl http://www.tightvnc.com/download/RfbPlayer-1.4.0.1.zip > $TMP/vncp.zip
+# unzip into temp folder
+unzip -q -d $TMP $TMP/vncp.zip
+
+# start player with given file
+java -jar $TMP/RfbPlayer-1.4.0.1/RfbPlayer.jar URL file:$*
+
+rm -rf $TMP
