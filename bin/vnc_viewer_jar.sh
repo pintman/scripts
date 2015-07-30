0724d299e4e25a441478ed58dac600237b65377a
Marco Bakera <Marco@Bakera.de>
2015-07-30 13:50:13 +0200

    new script for the tightvnc java viewer. including download script into temporary folder.

---
+++ bin/vnc_viewer_jar.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x
+# use -x for debugging
+
+TMP=$(mktemp -d)
+
+# download vnc viewer application with curl in silent mode
+curl -s http://www.tightvnc.com/download/1.3.10/tightvnc-1.3.10_javabin.zip > $TMP/vncv.zip
+# unzip into temp folder
+unzip -q -d $TMP $TMP/vncv.zip
+
+# start viewer
+#java -cp classes/VncViewer.jar VncViewer HOST localhost
+java -cp $TMP/classes/VncViewer.jar VncViewer HOST localhost PORT 5901
+
+
+rm -rf $TMP
