86b94af1b26f24da762c8c86c9345f8763b38223
Marco Bakera <Marco@Bakera.de>
2015-07-30 13:52:04 +0200

    no debugging and no silent download.

--- bin/vnc_viewer_jar.sh
+++ bin/vnc_viewer_jar.sh
@@ -1,10 +1,10 @@
-#!/bin/bash -x
+#!/bin/bash
 # use -x for debugging
 
 TMP=$(mktemp -d)
 
-# download vnc viewer application with curl in silent mode
-curl -s http://www.tightvnc.com/download/1.3.10/tightvnc-1.3.10_javabin.zip > $TMP/vncv.zip
+# download vnc viewer application with curl
+curl http://www.tightvnc.com/download/1.3.10/tightvnc-1.3.10_javabin.zip > $TMP/vncv.zip
 # unzip into temp folder
 unzip -q -d $TMP $TMP/vncv.zip
 
