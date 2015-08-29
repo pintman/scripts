863357dc8ddc289a55fecc7c956aa5e1135aea20
Marco Bakera <Marco@Bakera.de>
2015-08-29 16:59:03 +0200

    Added mc, cleanup ordering.

--- bin/win_default_install.bat
+++ bin/win_default_install.bat
@@ -1,21 +1,25 @@
 @echo off
 
-REM setup a default environment in a new and fresh windows installation.
+REM Setup a default environment in a new/fresh windows installation.
 
-REM install chocolately.org
+REM Iinstall chocolately.org
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 
-choco install chromium -y
+REM Tools
 choco install git -y
 choco install putty -y
-choco install pdfcreator -y
 choco install cygwin cyg-get -y
+cyg-get mc
+
+REM Applications
+choco install chromium -y
+choco install pdfcreator -y
 choco install wireshark -y
 choco install foxitreader -y
 choco install totalcommander -y
-choco install wget -y
 choco install visualstudiocommunity2013 -y
 
+choco install wget -y
 wget http://releases.0x539.de/gobby/gobby-stable.exe
 start /wait gobby-stable.exe
 del gobby-stable.exe
