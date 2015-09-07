b6a0d402d4821eab5842ee1bea1a796432226a8c
Marco Bakera <Marco@Bakera.de>
2015-09-07 12:33:44 +0200

    no groups creation needed.

--- bin/create_accounts.sh
+++ bin/create_accounts.sh
@@ -3,9 +3,9 @@
 PASS=geheimnis
 
 USERS=$(cat << EOF
-ita1
-ita2
-ita3
+username1
+username2
+username3
 EOF
 )
 
@@ -13,8 +13,7 @@ echo Adding users with password $PASS
 
 for u in $USERS; 
 do
-  groupadd $u
-  useradd -m -g $u -s /bin/bash $u
+  useradd -m -s /bin/bash $u
   bash -c "echo $u:$PASS | chpasswd"
   echo $u added
 done
