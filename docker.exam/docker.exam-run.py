d111da915822ada51d9051584d3b01f1f1d9d07b
Marco Bakera <Marco@Bakera.de>
2015-12-27 16:44:25 +0100

    new file for starting containers.

---
+++ docker.exam/docker.exam-run.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+# Run containers
+
+import sys
+import os
+
+portprefix = "5"
+
+# checking command line arguments
+if len(sys.argv) != 2:
+  print("Give number of containers to be created")
+  exit(1)
+
+anzahl = int(sys.argv[1])
+if anzahl >= 10:
+  print("Maximum of 10 containers allowed")
+  exit(1)
+
+# Starting containers exam0, ..., examN
+print(" Starting " + str(anzahl) + " containers and mapping ports")
+
+for i in range(1, anzahl + 1):
+  # ii = format("%02.0f" % i) # format i to exactly two places
+  ii = str(i)
+  portmap = (
+    " -p " + portprefix + ii + "022:22"
+    " -p " + portprefix + ii + "080:80"
+    " -p " + portprefix + ii + "443:443 "
+  )
+  print(" Starting exam" + ii)
+  os.system("docker run -d --name=exam" + ii + portmap + " exam")
+  os.system("docker port exam" + str(i))
+
