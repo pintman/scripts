265ab976bc34949791023450876088f07fb4cb2d
Marco Bakera <Marco@Bakera.de>
2015-12-30 20:25:48 +0100

    minor doc

--- docker.exam/docker.exam-run.py
+++ docker.exam/docker.exam-run.py
@@ -18,15 +18,14 @@ anzahl = int(sys.argv[1])
 print(" Starting " + str(anzahl) + " containers and mapping ports")
 
 for i in range(1, anzahl + 1):
-  # ii = format("%02.0f" % i) # format i to exactly two places
   ii = basePort + 100 * i
   portmap = (
     " -p " + str(ii+22) + ":22"
     " -p " + str(ii+80) + ":80"
     " -p " + str(ii+43) + ":443 "
   )
-  print(" Starting exam" + str(i))
   containerName = "exam" + str(i)
+  print(" => Starting exam" + containerName)
   os.system("docker run -d -h " + containerName + " --name=" + containerName + portmap + " exam")
   os.system("docker port " + containerName)
 
