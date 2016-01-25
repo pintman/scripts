822fa5bac17c9998766785a7dae4352f68f4d52b
Marco Bakera <Marco@Bakera.de>
2016-01-25 13:14:29 +0100

    choco install auf eine Zeile zusammengefasst, XAMMP neue Version

--- bin/win_default_install.bat
+++ bin/win_default_install.bat
@@ -6,19 +6,11 @@ REM Install chocolately.org
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 
 REM Tools
-choco install git -y
-choco install putty -y
-choco install cygwin cyg-get -y
+choco install cygwin cyg-get git putty  -y
 cyg-get mc
 
 REM Applications
-choco install notepadplusplus -y
-choco install chromium -y
-choco install wireshark -y
-choco install foxitreader -y
-choco install totalcommander -y
-choco install visualstudiocommunity2013 -y
-choco install sharpdevelop -y
+choco install chromium foxitreader notepadplusplus sharpdevelop visualstudiocommunity2013 wireshark -y
 REM choco install unity -y
 REM choco install unity4 -y
 
@@ -27,8 +19,8 @@ wget http://releases.0x539.de/gobby/gobby-stable.exe
 start /wait gobby-stable.exe
 del gobby-stable.exe
 
-wget https://www.apachefriends.org/xampp-files/5.6.12/xampp-win32-5.6.12-0-VC11-installer.exe
-start /wait xampp-win32-5.6.12-0-VC11-installer.exe
-del xampp-win32-5.6.12-0-VC11-installer.exe
+wget https://www.apachefriends.org/xampp-files/5.6.15/xampp-win32-5.6.15-1-VC11-installer.exe
+start /wait xampp-win32-5.6.15-1-VC11-installer.exe
+del xampp-win32-5.6.15-1-VC11-installer.exe
 
 pause
