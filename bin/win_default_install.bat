d86049b30003d1da85e17d28815ede953d4c390c
Marco Bakera <Marco@Bakera.de>
2016-03-22 05:40:01 +0100

    added winscp, python3

--- bin/win_default_install.bat
+++ bin/win_default_install.bat
@@ -6,7 +6,7 @@ REM Install chocolately.org
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 
 REM Tools
-choco install cygwin cyg-get git putty  -y
+choco install cygwin cyg-get git putty python3 winscp -y
 cyg-get mc
 
 REM Applications
