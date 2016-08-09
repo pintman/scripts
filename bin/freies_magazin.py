0cd19c9dd8d6241eff670c6bf46e8f08b628fab9
Marco Bakera <Marco@Bakera.de>
2016-08-09 20:24:29 +0200

    Dokumentation und doctests hinzu.

--- bin/freies_magazin.py
+++ bin/freies_magazin.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-#
-# Ein einfaches Skript, das die Ausgaben des Linux-Magazins "freies Magazin"
-# nach einem Stichwort durchsucht. Es werden Titel und Zusammenfassungen der
-# Artikel berücksichtigt.
-# 
-# Der folgende Beispielaufruf liefert alle Artikel, in den Python thematisiert wird.
-#
-# ./freies_magazin.py Python
-#
-# 
-#
+
+"""Ein einfaches Skript, das die Ausgaben des Linux-Magazins "freies Magazin"
+nach einem Stichwort durchsucht.
+
+Es werden Titel und Zusammenfassungen der Artikel berücksichtigt.
+ 
+Der folgende Beispielaufruf liefert alle Artikel, in den Python thematisiert wird.
+
+./freies_magazin.py Python
+"""
 
 import xml.etree.ElementTree as ET
 import urllib.request
@@ -22,6 +21,23 @@ def suche(suchwort):
     """
     Durchsuche den Suchbaum ab root nach dem suchwort. Suche in summary und
     title von Artikeln.
+
+    >>> import freies_magazin
+    >>> freies_magazin.suche('Handy')
+    Welches Betriebssystem ist das richtige für Netbooks? (Ausgabe 2010-06)
+    http://www.freiesmagazin.de/freiesmagazin-2010-06
+    Symbian für Schlangenbeschwörer (Ausgabe 2010-09)
+    http://www.freiesmagazin.de/freiesmagazin-2010-09
+    GnuCash (Ausgabe 2012-08)
+    http://www.freiesmagazin.de/freiesmagazin-2012-08
+
+    >>> freies_magazin.suche('Docker im Schuleinsatz')
+    Docker im Schuleinsatz (Ausgabe 2016-06)
+    http://www.freiesmagazin.de/freiesMagazin-2016-06
+
+    >>> freies_magazin.suche('docker im schuleinsatz')
+    Docker im Schuleinsatz (Ausgabe 2016-06)
+    http://www.freiesmagazin.de/freiesMagazin-2016-06
     """
     xml = urllib.request.urlopen(base_url)
     tree = ET.parse(xml)
@@ -38,11 +54,12 @@ def suche(suchwort):
             if title is None:
                 title = ""
 
-            if  suchwort in summary or suchwort in title:
+            if  suchwort.lower() in summary.lower() or suchwort.lower() in title.lower():
                 url = issue.find(NS+"url").text
                 print("%s (Ausgabe %s)\n%s" % (title, number, url))
 
-if __name__ == "__main__":
+
+def main():
     if len(sys.argv) < 2:
         print("Ich brauche ein Suchwort.")
         exit(1)
@@ -53,3 +70,6 @@ if __name__ == "__main__":
 
     suche(suchwort.strip())
 
+
+if __name__ == "__main__":
+    main()
