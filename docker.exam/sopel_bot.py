fdbc590992c3c2e27eb6b45363ad55144eb17232
Marco Bakera <marco@bakera.de>
2016-08-20 23:14:36 +0200

    Doc.

--- docker.exam/sopel_bot.py
+++ docker.exam/sopel_bot.py
@@ -1,14 +1,13 @@
+"""Ein Bot, der von dem IRC-Bot Sopel benutzt werden kann, um die Container zu
+starten."""
+
 from sopel import module
-#
-# Ein Bot, der von dem IRC-Bot Sopel benutzt werden kann, um die Container zu
-# starten.
-#
 
 @module.commands('docker_exam')
 @module.require_admin("Hier dürfen nur Admins ran.")
 def docker_exam(bot, trigger):
-    """
-    Erlaubt das Starten und Verwalten von docker-Containern.
+    """Erlaubt das Starten und Verwalten von docker-Containern.
+
     .docker_exam run anzahl=3 - Starte 3 Container
     .docker_exam show  - zeige laufende Container
     .docker_exam stop  - stoppt alle Container
@@ -19,11 +18,11 @@ def docker_exam(bot, trigger):
     anzahl = 0
     argumente = trigger.group(2)
     method = argumente.split(' ')[0]
-    #print("method", method)
+    # print("method", method)
     parameter = argumente.split(' ')[1:]
-    #print("parameter", parameter)
+    # print("parameter", parameter)
         
-    #print("werte Parameter aus")
+    # print("werte Parameter aus")
     for param in parameter:
         if method == "run" and "anzahl=" in param:
             anzahl = int(param.split('=')[1])
