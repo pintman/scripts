9037ba43690bd73c7e148c3e848afcccd686aedf
Marco Bakera <marco@bakera.de>
2016-10-26 18:57:02 +0200

    Fehler behoben

--- bin/useradd.isolated.sh
+++ bin/useradd.isolated.sh
@@ -2,15 +2,23 @@
 
 USER=exam
 
-if [[ grep $USER /etc/pass ]];
+if [[ $(whoami) != "root" ]];
+then
+	echo "Bitte mit root-Rechten ausführen - z.B. mit sudo.";
+	exit 1;
+fi
+
+if grep $USER /etc/passwd ; 
+then
 	echo "User existiert bereits!";
 	exit 1;
 fi
 
-sudo useradd -m $USER
+echo "User wird angelegt."
+useradd -m $USER
 echo "Passwort für den neuen User eingeben (und merken)"
-sudo passwd $USER
+passwd $USER
 
 
-sudo chmod go-x /home/*
+chmod go-x /home/*
 
