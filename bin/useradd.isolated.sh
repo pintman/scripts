2be8905bc2c18eb6c7bb2096b7fccc23e19ca519
Marco Bakera <marco@bakera.de>
2016-10-26 19:08:22 +0200

    Kommentare.

--- bin/useradd.isolated.sh
+++ bin/useradd.isolated.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
+#
+# TODO Wird sudo benötigt? Falls ja, könnten für bestimmte Programme 
+# (python, python3, pydoc, ...) Rechte in sudoers eingeraeum werden.
+#
 
+# Username für den neuen User
 USER=exam$RANDOM
 
-# TODO Wird sudo benötigt? Falls ja, könnten für bestimmte Programme (python, python3, pydoc, ...) Recht in sudoers vergeben werden.
-
-if [[ $(whoami) != "root" ]];
+# Prüfen, ob das Skript mit Rootrechten ausgefuehrt wird.
+f [[ $(whoami) != "root" ]];
 then
 	echo "Bitte mit root-Rechten ausführen - z.B. mit sudo.";
 	exit 1;
 fi
 
+# Prüfen, ob der zu erstellende User bereits vorhanden ist.
 if grep $USER /etc/passwd ; 
 then
 	echo "User existiert bereits!";
 	exit 1;
 fi
 
+# Der neue User wird mit einem neuen Passwort angelegt.
 echo "User $USER wird angelegt."
 useradd -m $USER
 echo "Passwort für den neuen User $USER eingeben (und merken)"
