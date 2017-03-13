56dc5b571bd600189631b1b82e9786edc46a4226
Marco Bakera <bakera@tbs1.de>
2017-03-13 16:01:34 +0100

    added file to send mails autoamtically.

---
+++ bin/ausbilder-mail.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+import csv
+import os
+import tempfile
+
+configfile=os.environ["HOME"] + "/.ausbilder-mail.csv"
+template_file = os.environ["HOME"] + "/.ausbilder-mail.tmpl"
+template = "".join(open(template_file).readlines())
+betreff = "Versäumter Unterricht {schuelername}"
+
+
+class Kontakt:
+    def __init__(self, klasse, name, vorname, betrieb, ausbilder, mail):
+        self.klasse = klasse
+        self.name = name
+        self.vorname = vorname
+        self.betrieb = betrieb
+        self.ausbilder = ausbilder
+        self.ausbildermail = mail
+
+
+with open(configfile, "rt") as config:
+    rows = list(csv.reader(config, delimiter=";"))[1:]
+
+kontakte = []
+klassen = []
+for (i,row) in enumerate(rows):
+    kontakte.append(Kontakt(*row))
+
+    if row[0] not in klassen:
+        print(i, row[0])
+        klassen.append(row[0])
+
+klasse = klassen[int(input("Klasse? "))]
+
+for (i,kont) in enumerate(kontakte):
+    if kont.klasse == klasse:
+        print(i, kont.name, kont.vorname)
+
+schueler = kontakte[int(input("Schüler? "))]
+schueler_txt = schueler.vorname + " " + schueler.name
+
+daten = input("Daten? ")
+
+
+to=schueler.ausbildermail
+betreff = betreff.format(schuelername=schueler_txt)
+
+content = template.format(schuelername=schueler_txt, daten=daten, ausbilder=schueler.ausbilder)
+tmp_file = tempfile.NamedTemporaryFile(mode="wt", delete=False)
+tmp_file.write(content)
+tmp_file.close()
+
+mail_cmd = "mutt -b bakera@tbs1.de -s \"{betreff}\" -i {msg_file} {to}".format(
+    betreff=betreff,
+    to=to,
+    msg_file=tmp_file.name)
+print(mail_cmd)
+os.system(mail_cmd)
+os.remove(tmp_file.name)
