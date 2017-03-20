4ea62aaf22e25e3420157e494490194441d1c5e6
Marco Bakera <bakera@tbs1.de>
2017-03-20 18:19:01 +0100

    formatting output

--- bin/git-stat.sh
+++ bin/git-stat.sh
@@ -2,8 +2,7 @@
 
 for d in $*;
 do
-    echo "=== $d ===";
-    git -C $d remote get-url origin;
+    echo -en "=== $d (" $(git -C $d remote get-url origin) ") ==\n"
     git -C $d log --format=format:"%cr %cn| %s";
     find $d -type f -not -ipath *.git* -not -ipath *.idea* -exec wc -l {} \;
 done
