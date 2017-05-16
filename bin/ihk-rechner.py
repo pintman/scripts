a877e6a58dbad5f739531337062a534d22185b3e
Marco Bakera <marco@bakera.de>
2017-05-16 14:10:25 +0200

    added calculator for ihk note tables.

---
+++ bin/ihk-rechner.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import sys
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Maximale Punktzahl angeben")
+        exit(1)
+    max_p = int(sys.argv[1])
+    print("Note\tab")
+    print("----\t--")
+
+    for (note, faktor) in [(1, 0.92),
+                           (2, 0.81),
+                           (3, 0.67),
+                           (4, .5),
+                           (5, .3),
+                           (6, 0)]:
+        print(note, "\t", round(faktor*max_p, 1))
+
+
+if __name__ == '__main__':
+    main()
