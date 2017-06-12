a65bff0c754601864625e47f29a206d2ab9ca73c
Marco Bakera <marco@bakera.de>
2017-06-12 14:26:33 +0200

    added script for showing authors in git repo.

---
+++ bin/git-authors.sh
@@ -0,0 +1,13 @@
+#/bin/sh
+
+# for the given git Repo, list authors and the number of commits.
+
+if [[ -z $* ]]; then
+   echo "No Repo given."
+   exit 1
+fi
+
+echo -e "num commits     author"
+echo    "-----------     ------"
+git -C $* log | grep 'Author:' | sort | uniq -c
+
