bd34f1a4b7cbdaab13e2a64743ef4d036673a35d
Marco Bakera <marco@bakera.de>
2017-06-18 15:19:24 +0200

    added alternative ways for installing pkg in cygwin and python.

--- bin/win_default_install.bat
+++ bin/win_default_install.bat
@@ -9,18 +9,22 @@ REM Install chocolately.org
 REM Tools
 choco install cygwin cyg-get geany git putty python3 winscp win32diskimager -y
 cyg-get mc wget
+REM alternative: choco install mc wget --source cygwin
 
 REM upgrade pip (from python)
 python -m pip install --upgrade pip
+REM alternative: choco install pip --source python
 
 REM install eapi for the eamodule
 pip install eapi
+REM alternative: choco install eapi --source python
 
 REM Applications
 choco install 7zip foxitreader libreoffice notepadplusplus tightvnc wireshark -y
 
 REM Install scapy via pip - needs Pcap (via wireshark)
 pip install scapy-python3
+REM alternative: choco install scapy-python3 --source python
 
 wget https://www.apachefriends.org/xampp-files/5.6.30/xampp-win32-5.6.30-1-VC11-installer.exe
 start /wait xampp-win32-5.6.30-1-VC11-installer.exe
