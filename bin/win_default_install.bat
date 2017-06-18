2974fb1242506899123086b038ac765305ec8717
Marco Bakera <marco@bakera.de>
2017-06-18 15:10:50 +0200

    added wget,libreoffice and removed unity.

--- bin/win_default_install.bat
+++ bin/win_default_install.bat
@@ -1,13 +1,14 @@
 @echo off
 
 REM Setup a default environment in a new/fresh windows installation.
+REM This script must be run as administrator
 
 REM Install chocolately.org
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 
 REM Tools
 choco install cygwin cyg-get geany git putty python3 winscp win32diskimager -y
-cyg-get mc
+cyg-get mc wget
 
 REM upgrade pip (from python)
 python -m pip install --upgrade pip
@@ -16,13 +17,10 @@ REM install eapi for the eamodule
 pip install eapi
 
 REM Applications
-choco install 7zip foxitreader notepadplusplus tightvnc wireshark -y
+choco install 7zip foxitreader libreoffice notepadplusplus tightvnc wireshark -y
 
 REM Install scapy via pip - needs Pcap (via wireshark)
 pip install scapy-python3
-
-REM choco install unity -y
-REM choco install unity4 -y
 
 wget https://www.apachefriends.org/xampp-files/5.6.30/xampp-win32-5.6.30-1-VC11-installer.exe
 start /wait xampp-win32-5.6.30-1-VC11-installer.exe
