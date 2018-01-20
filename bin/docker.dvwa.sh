cf6a645b09b0970f61eaea9e15476116f2d2fb2b
Marco Bakera <marco@bakera.de>
2018-01-20 16:21:00 +0100

    switching to official docker container.

--- bin/docker.dvwa.sh
+++ bin/docker.dvwa.sh
@@ -1,22 +1,15 @@
 #!/bin/sh
 #
-# Based on https://hub.docker.com/r/citizenstig/dvwa/
-#
 
-MYSQL_PASS=wohnzimmer
-HTTP_PORT=50080
-MYSQL_PORT=53306
+HTTP_PORT=1080
 
 echo Starte Container.
 
 docker run \
   --name=dvwa -d \
-  -p $HTTP_PORT:80 -p $MYSQL_PORT:3306 \
-  -e MYSQL_PASS=$MYSQL_PASS citizenstig/dvwa
+  -p $HTTP_PORT:80 \
+  vulnerables/web-dvwa
 
 echo Portweiterleitungen:
 docker port dvwa
 
-echo Zugangsdaten
-echo "MySQL:        username=admin, password=$MYSQL_PASS"
-echo "WebAnwendung: username=admin, password=password"
