8fe0aba1554c8c7535af86b198c9a4644bdffc32
Marco Bakera <bakera@tbs1.de>
2018-06-17 11:03:30 +0200

    converting odt to pdf on server.

--- bin/buddybook_on.sh
+++ bin/buddybook_on.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
-USAGE="Usage: $0 file server"
+# fail fast
+set -o errexit
 
-FILE=${1?"File to be transferred is missing. $USAGE"}
+USAGE="Usage: $0 odt-file server"
+
+FILE_ODT=${1?"ODT-File to be transferred is missing. $USAGE"}
+FILE_PDF=$(basename $FILE_ODT .odt).pdf
 SERVER=${2?"Server is missing. $USAGE"}
 TGTDIR="/tmp"
-OUTFILE=pocketmod-$FILE
+OUTFILE=buddybook-$FILE_PDF
 
-echo Copy $FILE to $SERVER
-scp  $FILE $SERVER:$TGTDIR
-echo Convert on server $SERVER
-ssh $SERVER pdfjam-pocketmod $TGTDIR/$FILE --outfile $TGTDIR/$OUTFILE
+echo Copy $FILE_ODT to $SERVER:$TGTDIR
+scp  $FILE_ODT $SERVER:$TGTDIR
+
+echo Convert $TGTDIR/$FILE_ODT to $FILE_PDF on $SERVER
+ssh $SERVER libreoffice --headless --convert-to pdf --outdir $TGTDIR $TGTDIR/$FILE_ODT
+
+echo Convert to buddybook on server $SERVER
+ssh $SERVER pdfjam-pocketmod $TGTDIR/$FILE_PDF --outfile $TGTDIR/$OUTFILE
+
 echo copy result from $SERVER
 scp $SERVER:$TGTDIR/$OUTFILE .
-echo Removing result $OUTFILE on server $SERVER
-ssh $SERVER rm $TGTDIR/$OUTFILE
+
+echo Removing files on $SERVER
+ssh $SERVER rm $TGTDIR/$OUTFILE $TGTDIR/$FILE_ODT $TGTDIR/$FILE_PDF
