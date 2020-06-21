b14b906413766ca6df191283b52eeb2cfa42eb06
Marco Bakera <marco@bakera.de>
2020-06-21 09:34:05 +0200

    added viewport tag to make the page responsive.
    https://www.w3schools.com/css/css_rwd_viewport.asp

--- bin/fotoliste.py
+++ bin/fotoliste.py
@@ -15,6 +15,7 @@ HTML_TEMPLATE = """
 <head>
   <meta charset="UTF-8">
   <title>Fotoliste {title}</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/kognise/water.css@latest/dist/light.min.css">
 </head>
 <body>
