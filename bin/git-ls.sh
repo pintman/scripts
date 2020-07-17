20e6f2a5996e08cc39a910f5d6832795a5b84b03
Marco Bakera <marco@bakera.de>
2020-07-17 17:00:14 +0200

    added git-ls.

---
+++ bin/git-ls.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Output the given git folders in order of the last commit.
+#
+for d in $*; do 
+    # format options in section PRETTY FORMATS in git log's man page.
+    git -C $d log -n 1 --format="%ci $d" ;  
+done | sort
