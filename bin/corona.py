54a2f1c89c77050d8938f61aa6d945da1fb9122c
Marco Bakera <marco@bakera.de>
2020-10-23 15:24:36 +0200

    added previous value

--- bin/corona.py
+++ bin/corona.py
@@ -5,6 +5,7 @@ import urllib.request
 datasource = 'https://pavelmayer.de/covid/risks/data.csv'
 delimiter = ','
 fieldname_7tage_inzidenz = 'FaellePro100kLetzte7Tage'
+fieldname_7tage_inzidenz_davor = 'FaellePro100kLetzte7TageDavor'
 fieldname_landkreis = 'Landkreis'
 fieldname_meldung = 'MeldeDay'
 staedte = ['SK Bochum', 'SK Dortmund', 'SK Herne', 'SK Essen']
@@ -13,6 +14,7 @@ lines = urllib.request.urlopen(datasource).readlines()
 
 headline = str(lines[0]).split(delimiter)
 index_7tage_inzidenz = headline.index(fieldname_7tage_inzidenz)
+index_7tage_inzidenz_davor = headline.index(fieldname_7tage_inzidenz_davor)
 index_landkreis = headline.index(fieldname_landkreis)
 
 # mapping places to incidence
@@ -21,10 +23,12 @@ city_incidence = {}
 for line in lines[1:]:
     line_splitted = str(line).split(delimiter)
     landkreis = line_splitted[index_landkreis]
-    siebenT_inzidenz = line_splitted[index_7tage_inzidenz] 
-    city_incidence[landkreis] = float(siebenT_inzidenz)
+    siebenT_inzidenz = line_splitted[index_7tage_inzidenz]
+    siebenT_inzidenz_davor = line_splitted[index_7tage_inzidenz_davor]
+    city_incidence[landkreis] = float(siebenT_inzidenz), float(siebenT_inzidenz_davor)
 
 # output configured places
-print('Stadt    \t Infizierte/100kEinw. in 7 Tagen\n')
+print('Stadt    \t Infizierte/100kEinw. in 7 Tagen (davor)\n')
 for stadt in staedte:
-    print(stadt, '\t', round(city_incidence[stadt], 1))
+    inzidenz, inzidenz_davor = city_incidence[stadt]
+    print(f'{stadt}    \t {round(inzidenz, 1)}\t({round(inzidenz_davor, 1)})')
