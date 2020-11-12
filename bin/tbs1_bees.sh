29852fcba0a02fca464f01eccf658192f3a54896
Marco Bakera <marco@bakera.de>
2020-11-12 15:30:26 +0100

    formatting issue

--- bin/tbs1_bees.sh
+++ bin/tbs1_bees.sh
@@ -5,5 +5,5 @@ API='https://bees.tbs1.de/readmysql.php'
 today=$(date --iso)
 
 echo "Zeitstempel Gewicht(kg) Temperatur(°C) Luftfeuchtigkeit(%) Luftdruck(hPa) Gehäusetemperatur(°C)"
-lynx --dump $API | grep $today | head -n 1
+lynx --dump $API | grep $today | head -n 1 | sed -e 's/   //'
 
