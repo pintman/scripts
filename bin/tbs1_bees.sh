e8d69541b06489f4815894313dc5dc8a27581385
Marco Bakera <marco@bakera.de>
2020-11-12 17:20:38 +0100

    fix: date format for macos

--- bin/tbs1_bees.sh
+++ bin/tbs1_bees.sh
@@ -2,7 +2,7 @@
 
 API='https://bees.tbs1.de/readmysql.php'
 
-today=$(date --iso)
+today=$(date +%Y-%m-%d)
 
 echo "Zeitstempel Gewicht(kg) Temperatur(°C) Luftfeuchtigkeit(%) Luftdruck(hPa) Gehäusetemperatur(°C)"
 lynx --dump $API | grep $today | head -n 1 | sed -e 's/   //'
