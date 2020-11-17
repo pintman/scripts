c93feb13aa04b55e6c12978ac36d8e1ae737522c
Marco Bakera <marco@bakera.de>
2020-11-17 20:58:19 +0100

    more stats

--- bin/freifunk_node.py
+++ bin/freifunk_node.py
@@ -13,11 +13,13 @@ for node in js['nodes']:
     node_info = node['nodeinfo']
     node_id = node_info['node_id']
     if node_id == my_id:
-        fw = node_info['software']['firmware']['release']
         netaddr = node_info['network']['addresses']
-        clients = node['statistics']['clients']
-        hostname = node_info['hostname']
-        print('Hostname:', hostname)
-        print('Clients:', clients)
-        print('Firmware:', fw)
+        stat = node['statistics']
+        print('Hostname:', node_info['hostname'])
+        print('Clients:', stat['clients'])
+        print('Firmware:', node_info['software']['firmware']['release'])
         print('Adresses:', ', '.join(netaddr))
+        print('Average Load:', stat['loadavg'])
+        print('Uptime:', stat['uptime'])
+        print('Memory Usage:', stat['memory_usage'])
+        print('Online:', node['flags']['online'])
