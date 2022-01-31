ba78e397b820d16f2b00dec06934583761e0f558
Marco Bakera <marco@bakera.de>
2022-01-31 22:28:42 +0100

    select last known date

--- bin/tbs1_bees.sh
+++ bin/tbs1_bees.sh
@@ -2,8 +2,9 @@
 
 API='https://bees.tbs1.de/readmysql.php'
 
-today=$(date +%Y-%m-%d)
+# match iso date - e.g. 2022-01-31
+pattern='[0-9]{4}-[01][0-9]-[0-3][0-9]'
 
 echo "Zeitstempel Gewicht(kg) Temperatur(°C) Luftfeuchtigkeit(%) Luftdruck(hPa) Gehäusetemperatur(°C)"
-lynx --dump $API | grep $today | head -n 1 | sed -e 's/   //'
+lynx --dump $API | grep -E "$pattern" | head -n 1 | sed -e 's/   //'
 
