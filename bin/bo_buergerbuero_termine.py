731bf96143615f8f82ce5c69e9cecd2e25965514
Marco Bakera <bakera@tbs1.de>
2026-02-16 12:56:06 +0100

    wrapped into main func

--- bin/bo_buergerbuero_termine.py
+++ bin/bo_buergerbuero_termine.py
@@ -6,24 +6,30 @@ import json
 BASE = 'https://termine.bochum.de'
 SUB = '/m/buergerbuero/extern/calendar/search_result?search_mode=earliest&uid=eab3c2ce-bd9c-4c81-8dab-663aebdc0ce3&wsid=62e76b0e-3946-4152-917e-d0b6d9132901'
 
-URL = BASE + SUB
-html = requests.get(URL).text
 
-# looking for json in HTML
-#
-inJson = False
-js_str = ''
-for l in html.split():
-    if 'json_appointment_list' in l:
-        inJson = True
+def main():
+    URL = BASE + SUB
+    html = requests.get(URL).text
 
-    elif inJson and '</div>' in l:
-        inJson = False
+    # looking for json in HTML
+    #
+    inJson = False
+    js_str = ''
+    for l in html.split():
+        if 'json_appointment_list' in l:
+            inJson = True
 
-    elif inJson:
-        js_str += l + ' '
+        elif inJson and '</div>' in l:
+            inJson = False
 
-js = json.loads(js_str)
+        elif inJson:
+            js_str += l + ' '
 
-for app in js['appointments']:
-    print(app['date_time'], app['unit'], '\n', BASE + app['link'])
+    js = json.loads(js_str)
+
+    for app in js['appointments']:
+        print(app['date_time'], app['unit'], '\n', BASE + app['link'])
+
+if __name__ == "__main__":
+    main()
+    
