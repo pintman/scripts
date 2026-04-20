04127d812ac5ce80f1783724172d3b3556f497e5
Marco Bakera <bakera@tbs1.de>
2026-04-20 11:26:55 +0200

    added klausur check script

---
+++ bin/klausur_check.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+set -xe
+
+VENVDIR=~/tmp/venv
+
+python3 -m venv $VENVDIR
+. $VENVDIR/bin/activate
+
+pip install ruff findlike
+
+# -s Cause find to traverse the file hierarchies in lexicographical
+# -order, i.e., alphabetical order within each directory.
+
+find \
+     -s \
+     . \
+     -name "*py" \
+     -exec ls {} ";" \
+     -execdir ruff check -q {} ";" \
+     > check.log
+
+find -s . \
+     -name "*py" \
+     -exec echo "---- {}" ";" \
+     -exec findlike \
+       --show-scores --recursive \
+       --filename-pattern "*py" --threshold 0.8 \
+       --prefix "   " --hide-reference "{}" ";" \
+     > similarities.log
+
